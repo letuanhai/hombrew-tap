478e423df9f694f07d0c51154ba88b68029471cb
Le Tuan Hai <letuanhaiph@gmail.com>
2024-03-27 14:27:59 +0000

    fasd-clvv: add 1.0.1 bottle.

--- Formula/fasd-clvv.rb
+++ Formula/fasd-clvv.rb
@@ -6,6 +6,12 @@ class FasdClvv < Formula
   license "MIT"
   head "https://github.com/clvv/fasd.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/letuanhai/homebrew-tap/releases/download/fasd-clvv-1.0.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "bdb2d29aa930ef614864069ad364d61e9428b6414d3bd5f21def24342c4fbfb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "70d5a6db64a51fcaa6c229583b8821b42695757d391d92b094231268e620eec9"
+  end
+
   def install
     bin.install "fasd"
     man1.install "fasd.1"
